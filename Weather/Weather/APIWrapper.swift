da398335eb4c2751b9d756fb52c766c03792cbe0
Zontan Fotland <zontan@gmail.com>
2019-02-01 10:31:38 -0800

    [STORY-2] retrieve data from API

---
+++ Weather/Weather/APIWrapper.swift
@@ -0,0 +1,62 @@
+//
+//  APIWrapper.swift
+//  Weather
+//
+//  Created by Jonathan  Fotland on 2/1/19.
+//  Copyright © 2019 Jonathan Fotland. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+enum APIError: Error {
+    case invalidCoordinates, jsonParseError, serverError
+}
+
+class APIWrapper {
+    static let sharedInstance = APIWrapper()
+    
+    private let baseURL = "https://api.darksky.net"
+    //If this were a full app I wouldn't check in the API key like this to a public repo, but if this were a full app I wouldn't be using a public repo.
+    private let APIKey = "e0cea2af294b25e13e92019908ffc6eb"
+    
+    //https://api.darksky.net/forecast/e0cea2af294b25e13e92019908ffc6eb/37.8267,-122.4233
+    
+    private func constructURLRequest(endpoint: String, coordinates: String) -> URLRequest? {
+        guard let url = URL(string:"\(baseURL)/\(endpoint)/\(APIKey)/\(coordinates)") else {
+        
+            return nil
+        }
+        
+        return URLRequest(url: url)
+    }
+    
+    private init() {}
+    
+    func getForecast(for location: CLLocation, callback: @escaping (Any?, APIError?) -> Void) {
+        let endpoint = "forecast"
+        let lat = location.coordinate.latitude
+        let lon = location.coordinate.longitude
+        let coords = "\(lat),\(lon)"
+        
+        if let urlRequest = constructURLRequest(endpoint: endpoint, coordinates: coords) {
+            let task = URLSession.shared.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
+                guard error == nil, let data = data else {
+                    //Obviously we could make this error handling more robust if we needed to. For now, this is probably fine.
+                    callback(nil, APIError.serverError)
+                    return
+                }
+                do {
+                    let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+                    callback(jsonObject, nil)
+                } catch {
+                    //Invalid JSON response - nothing we can do
+                    callback(nil, APIError.jsonParseError)
+                }
+            })
+            task.resume()
+        } else {
+            callback(nil, APIError.invalidCoordinates)
+        }
+    }
+}
--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -12,6 +12,7 @@ import CoreLocation
 class ViewController: UIViewController {
 
     @IBOutlet weak var locationField: UITextField!
+    @IBOutlet weak var goButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,37 +23,51 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func showError(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        goButton.isEnabled = true
+        show(alert, sender: nil)
+    }
 
     @IBAction func didPressGo(_ sender: Any) {
-        if let location = locationField.text, location != "" {
-            getCoordinates(location: location)
+        goButton.isEnabled = false //Disable the button while we're working
+        if let locationString = locationField.text, locationString != "" {
+            getCoordinates(location: locationString) { (location) in
+                APIWrapper.sharedInstance.getForecast(for: location) { (data, error) in
+                    if let error = error {
+                        switch error {
+                        case .invalidCoordinates:
+                            self.showError(message: "Could not get coordinates for that location. Try entering a more exact address, city, or zip code.")
+                        case .jsonParseError:
+                            self.showError(message: "Error reading weather data. Apologies for the inconvenience.")
+                        case .serverError:
+                            self.showError(message: "Could not reach weather forecast server. Please try again later.")
+                        }
+                    } else if let data = data {
+                        //TODO: Parse the data, show results
+                    }
+                }
+            }
         } else {
-            let alert = UIAlertController(title: "Error", message: "Please enter a location", preferredStyle: .alert)
-            
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            
-            show(alert, sender: nil)
+            showError(message: "Please enter a location")
         }
     }
     
-    func getCoordinates(location: String) {
+    func getCoordinates(location: String, callback: @escaping (CLLocation) -> Void) {
     
         let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(location) {
-            placemarks, error in
+        geocoder.geocodeAddressString(location) { (placemarks, error) in
             let placemark = placemarks?.first
-            guard let lat = placemark?.location?.coordinate.latitude,
-                let lon = placemark?.location?.coordinate.longitude else {
-                    let alert = UIAlertController(title: "Error", message: "Could not understand your location. Make sure everything is spelled correctly.", preferredStyle: .alert)
-                    
-                    print(error?.localizedDescription)
-                    
-                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                    
-                    self.show(alert, sender: nil)
+            guard let location = placemark?.location else {
+                self.showError(message: "Could not understand your location. Make sure everything is spelled correctly.")
+                
                 return
             }
-            print("Lat: \(lat), Lon: \(lon)")
+            callback(location)
         }
     }
     
